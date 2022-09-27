dc8a8b4c0c3a1eaa0b4194f7c5d896d4b9f1816d
Kishor  Desai <114481961+GitKDCollege@users.noreply.github.com>
2022-09-27 11:16:35 +0000

    Problem03 solved

---
+++ set01/problem03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+  int a,b;
+  printf("Enter first number\n");
+  scanf("%d",&a);
+  printf("Enter second number\n");
+  scanf("%d",&b);
+  int sum;
+  sum=a+b;
+  printf("Sum is %d",sum);
+  return 0;
+}
